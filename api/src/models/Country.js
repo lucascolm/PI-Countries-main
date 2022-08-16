3560c1bd1addba23f1f3d31a3119696083704126
lucascolm <lucaspaganelli20@gmail.com>
2022-08-15 23:43:06 -0300

    creo-modelo-Country

--- api/src/models/Country.js
+++ api/src/models/Country.js
@@ -4,9 +4,41 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('country', {
+    id:{
+      type:DataTypes.STRING(3),
+      allowNull:false,
+      unique:false,
+      primaryKey:true
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    flag:{
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    
+    continent:{
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    capital:{
+      type: DataTypes.STRING,
+      
+    },
+    subregion:{
+      type: DataTypes.STRING,
+      
+    },
+    area:{
+      type: DataTypes.FLOAT,
+    },
+    population:{
+      type: DataTypes.INTEGER,
+    }
+
+  },{
+    timestamps:false
   });
 };
