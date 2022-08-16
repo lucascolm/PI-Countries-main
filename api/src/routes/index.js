22036ec4ebe13aadf719aece3f1f6d7fd437d153
lucascolm <lucaspaganelli20@gmail.com>
2022-08-16 15:49:51 -0300

    configuro-ruteo

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,10 +1,12 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
+const routerCountries= require('./country');
+const routerActivity= require('./activity');
 
 const router = Router();
-
+router.use("/countries", routerCountries);
+router.use("/activity",routerActivity);
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
