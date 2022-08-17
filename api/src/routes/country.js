74b9b83864ed41600a1962b6326d0b77f9cabd12
lucascolm <lucaspaganelli20@gmail.com>
2022-08-17 16:27:11 -0300

    agrego-ruta-post

--- api/src/routes/country.js
+++ api/src/routes/country.js
@@ -1,31 +1,79 @@
 const{Router}=require('express')
 const router=Router();
 const{Country,Activity}=require('../db');
-const{op}=require('sequelize')
+const{Op}=require('sequelize')
 const axios=require('axios');
 
-router.get('/',async(req,res)=>{
-    
-    const apiCountries= await axios.get("https://restcountries.com/v3/all?_limit=10")
-    const result=apiCountries.data;
-    const Country= await result.map(c=>{
-        let obj={
-            id:c.cca3,
-            name:c.name.common,
-            flag:c.flags[0],
-            continent:c.region,
-            capital:c.capital?c.capital[0]:"no capital",
-            subregion:c.subregion,
-            area:parseInt(c.area),
-            population:c.population,
 
+    const getCountry=async()=>{
+        let dbCountry=await Country.findAll();
+        try {
+            if(dbCountry.length>0){
+                
+                return dbCountry
+             }
+             const auxCountries= await axios.get("https://restcountries.com/v3/all")
+             const countries= auxCountries.data.map(c=>{
+                 return{
+                     id:c.cca3,
+                     name:c.name.common,
+                     flag:c.flags[0],
+                     continent:c.region,
+                     capital:c.capital?c.capital[0]:"undefined capital",
+                     subregion:c.subregion?c.subregion:"undefined subregion",
+                     area:c.area,
+                     population:c.population,
+                 }
+             })
+             
+             await Country.bulkCreate(countries)
+             
+         return countries;
+        } catch (error) {
+            console.log('Error controllers getCountries ' + error) 
         }
-        return obj;
-        
-    })
-    return res.status(201).json(Country)
-})
+          
+            
+           
+   
+    }
 
+
+router.get('/',async(req,res)=>{
+    console.log("entra a get")
+    const {name}=req.query;
+    try {
+        const countries =  await getCountry();
+        if(name){
+           const byNameCountries= await Country.findAll({where:{
+            name:{
+                [Op.iLike] : `%${name}%`
+            }
+           }})
+           return byNameCountries.length>0?res.status(201).json(byNameCountries):res.status(201).send('no encontro paises')
+        }else{
+            //const countries =  await getCountry();
+            return res.status(201).json(countries)
+        }
+    } catch (error) {
+        res.status(400).send('error: ' + error.message)
+    }
+})
+router.get('/:id',async (req,res)=>{
+    console.log("holaaaaaaaaaaa")
+    const {id} = req.params
+    try {
+        const countries =  await getCountry();
+    const byIdCountries= await Country.findByPk(id.toUpperCase(),{include:[Activity]})
+    if(byIdCountries){
+        res.status(201).json(byIdCountries) 
+    }else{
+        res.status(404).send("pais no encontrado")
+    }
+    } catch (error) {
+        res.status(404).send(`id invalido `)
+    }
+})
 
 
 
