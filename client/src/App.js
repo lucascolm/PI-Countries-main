34ad013f38a6956b5c5a35609b6e8607368c42ce
lucascolm <lucaspaganelli20@gmail.com>
2022-08-21 23:05:48 -0300

    configuro ruteo de la app

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
-import './App.css';
+import "./App.css";
+import React from "react";
+import { Route, BrowserRouter,Switch} from "react-router-dom";
+
+import Landing from "./Components/Landing/Landing";
 
 function App() {
   return (
     <div className="App">
       <h1>Henry Countries</h1>
+      <BrowserRouter>
+      |<Switch>
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/home" />
+        <Route exact path="/countrie/:id" />
+        <Route exact path="/activity" />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
