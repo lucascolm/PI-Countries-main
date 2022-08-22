290ba69e1a5d3347ce07d926b59cf36b9f048775
lucascolm <lucaspaganelli20@gmail.com>
2022-08-21 23:04:57 -0300

    creo el componente Landing

---
+++ client/src/Components/Landing/Landing.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+// export default function Landing() {
+//   return (
+//     <div>Landing</div>
+//   )
+// }
+const Landing = () => {
+  return <div></div>;
+};
+export default Landing;
