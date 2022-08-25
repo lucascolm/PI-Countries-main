2e3ae0bf1f9a8729507d1355727576b122fded52
lucascolm <lucaspaganelli20@gmail.com>
2022-08-24 23:29:17 -0300

    creo mi reducer

--- client/src/redux/reducer/index.js
+++ client/src/redux/reducer/index.js
@@ -1,14 +1,53 @@
+import { GET_ALL_COUNTRIES,GET_COUNTRIES_BY_NAME,GET_COUNTRIES_BY_ID,POST_ACTIVITY,REMOVE_SEARCH,CLEAN_STATE, isLoading} from "../actions";
 const initialState ={
-    allContries:[],
+    allCountries:[],
     countryById:{},
     contriesByName:[],
-    activitis:[]   
+    activitis:[] ,  
+    isLoading:false
 };
 
 
+  function rootReducer(state=initialState, action){
+    switch(action.type){
+        case GET_ALL_COUNTRIES:
+            return{
+                ...state,
+                allCountries:action.payload
+            }
+        case GET_COUNTRIES_BY_NAME:
+            return{
+                ...state,
+                contriesByName:action.payload
+            } 
+        case GET_COUNTRIES_BY_ID:
+            return{
+                ...state,
+                countryById:action.payload
 
-export default  function rootReducer(state=initialState, action){
+            }   
+        case POST_ACTIVITY:
+            return{
+              ...state  
+            }
+        case REMOVE_SEARCH:
+            return{
+                ...state,
+                contriesByName:[]
+            }
+        case CLEAN_STATE:
+            return{
+                ...state,
+                countryById:{}
+            }  
+        case "IS_LOADING":
+            return{
+                ...state,
+                
+            }
+        default:return state
+    }
     
 }
 
-
+export default rootReducer;
