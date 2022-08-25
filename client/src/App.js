fe7429dfcbd1104cfb49c81247f5bf6c9b51fc56
lucascolm <lucaspaganelli20@gmail.com>
2022-08-24 23:24:26 -0300

    update en el ruteo

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,25 @@
 import "./App.css";
 import React from "react";
-import { Route, BrowserRouter,Switch} from "react-router-dom";
+import { Route, BrowserRouter, Switch } from "react-router-dom";
 
 import Landing from "./Components/Landing/Landing";
-
+import Home from "./Components/Home/Home";
+import CardDetail from "./Components/CardDetail/CardDetail";
+import NavBar from "./Components/NavBar/NavBar";
+import FormAct from "./Components/FormAct/FormAct";
 function App() {
   return (
     <div className="App">
-      <h1>Henry Countries</h1>
       <BrowserRouter>
-      |<Switch>
-        <Route exact path="/" component={Landing} />
-        <Route exact path="/home" />
-        <Route exact path="/countrie/:id" />
-        <Route exact path="/activity" />
-        </Switch>
+        {/* <Switch> */}
+          <Route exact path="/" component={Landing} />
+          <Route path="/home"component={NavBar}/>
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/home/countries/:id" component={CardDetail} />
+          <Route exact path="/home/activity" component={FormAct} />
+          
+         
+        {/* </Switch> */}
       </BrowserRouter>
     </div>
   );
