162dee660601c40fa26ca7bd117f9290d2945160
lucascolm <lucaspaganelli20@gmail.com>
2022-08-24 23:27:36 -0300

    creo mi componente landing

--- client/src/Components/Landing/Landing.jsx
+++ client/src/Components/Landing/Landing.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import{Link} from 'react-router-dom'
 
-// export default function Landing() {
-//   return (
-//     <div>Landing</div>
-//   )
-// }
-const Landing = () => {
-  return <div></div>;
-};
-export default Landing;
+export default function Landing() {
+  return (
+    <div>Landing
+    <Link to="/home">
+    <button>Home</button>
+    </Link>
+    </div>
+  )
+}
+// const Landing = () => {
+//   return <div></div>;
+// };
+// export default Landing;
