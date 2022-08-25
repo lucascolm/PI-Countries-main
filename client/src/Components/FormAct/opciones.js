19bdff1bb03cbf8cba73c2f5c6699a2d4ee124d7
lucascolm <lucaspaganelli20@gmail.com>
2022-08-24 23:26:33 -0300

    creo un archivo aux(no uso)

---
+++ client/src/Components/FormAct/opciones.js
@@ -0,0 +1,7 @@
+const opciones={
+  temporadas:[{ label: "Verano", value: "Summer" },
+  { label: "Otoño", value: "Autumn" },
+  { label:"Invierno", value: "Winter" },
+  { label: "Primavera", value: "Spring" },]
+}
+export default opciones;
