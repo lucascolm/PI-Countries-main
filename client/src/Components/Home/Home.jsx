003af90716d8e882282290fb5b9015ff98f88617
lucascolm <lucaspaganelli20@gmail.com>
2022-08-29 13:27:46 -0300

    fix paginado con funcion extra

--- client/src/Components/Home/Home.jsx
+++ client/src/Components/Home/Home.jsx
@@ -1,69 +1,93 @@
-import React,{ useEffect,useState } from 'react'
-import{useDispatch,useSelector}from "react-redux"
-import { getAllCountries,removeSearch } from '../../redux/actions'
-import Card from "../Card/Card"
-import Paginado from '../Paginado/Paginado'
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllCountries, removeSearch } from "../../redux/actions";
+import Card from "../Card/Card";
+import paginado from "./funciones";
+import s from "./Home.module.css";
+
 //import NavBar from '../NavBar/NavBar'
 
 const Home = () => {
- const auxCountries=useSelector((state)=>state.allCountries)
- const countriesN=useSelector((state)=>state.contriesByName)
- const dispatch=useDispatch()
-  
-//Paginado
-const[page,setPage]=useState(1)
-const showPerPage=10;
-const lastOnPage=page * showPerPage;
-const firstOnPage=lastOnPage-showPerPage;
-console.log(firstOnPage,lastOnPage)
+  const auxCountries = useSelector((state) => state.allCountries);
+  const countriesN = useSelector((state) => state.contriesByName);
+  const dispatch = useDispatch();
 
-const showCountries=auxCountries.slice(firstOnPage,lastOnPage);
-// console.log(showCountries)
+  const [page, setPage] = useState(1);
+  //Paginado
 
-function paginado(pageNumber){
-  console.log(showCountries)
-  return setPage(pageNumber)
-}
+  //console.log(firstOnPage,lastOnPage)
 
+  // const showCountries=auxCountries.slice(firstOnPage,lastOnPage);
+  // console.log(showCountries)
 
- useEffect(()=>{
-  dispatch(removeSearch())
-  dispatch(getAllCountries())
+  useEffect(() => {
+    dispatch(removeSearch());
+    dispatch(getAllCountries());
+  }, [dispatch]);
 
- },[dispatch])
-
-{/* <Paginado showPerPage={showPerPage} allCountries={auxCountries.length} paginado={paginado}/> */}
   return (
-    <div className='home'>
-      
-      {
-      countriesN.length>0?typeof countriesN=='string'?
-      <p>no se encontro el pais</p>:
-      countriesN.map(c=>{
-        return(
-        <div key={c.id}>
-        <Card
-        key={c.id}
-        id={c.id}
-        name={c.name}
-        flag={c.flag}
-        continent={c.continent}
-        />
-        </div>
-      )})
-      :  showCountries.length>0? showCountries.map(c=>
-          <Card
-          key={c.id}
-          id={c.id}
-          name={c.name}
-          flag={c.flag}
-          continent={c.continent}
-          />
-         
-        
-      ):"no hay paises" }
-    <Paginado showPerPage={showPerPage} allCountries={auxCountries.length} paginado={paginado}/>
+    <div className={s.home}>
+      {/* {!auxCountries?null:<NavBar/>} */}
+      <div className={s.cards}>
+        {countriesN.length > 0 ? (
+          typeof countriesN == "string" ? (
+            <p>no se encontro el pais</p>
+          ) : (
+            paginado(countriesN)[page - 1].paises.map((c) => {
+              return (
+                <div key={c.id}>
+                  <Card
+                    key={c.id}
+                    id={c.id}
+                    name={c.name}
+                    flag={c.flag}
+                    continent={c.continent}
+                  />
+                </div>
+              );
+            })
+          )
+        ) : auxCountries.length > 0 ? (
+          paginado(auxCountries)[page - 1].paises.map((c) => (
+            <Card
+              key={c.id}
+              id={c.id}
+              name={c.name}
+              flag={c.flag}
+              continent={c.continent}
+            />
+          ))
+        ) : (
+          "no hay paises"
+        )}
+      </div>
+      <div className={s.paginado}>
+        {countriesN.length > 0 && typeof countriesN !== "string"
+          ? paginado(countriesN).map((e, index) => {
+              return (
+                <button
+                  onClick={() => {
+                    setPage(index + 1);
+                  }}
+                >
+                  {index + 1}
+                </button>
+              );
+            })
+          : auxCountries.length > 0 &&
+            paginado(auxCountries).map((e, index) => {
+              return (
+                <button
+                  onClick={() => {
+                    setPage(index + 1);
+                  }}
+                >
+                  {index + 1}
+                </button>
+              );
+            })}
+      </div>
     </div>
-  )
-}
+  );
+};
 export default Home;
