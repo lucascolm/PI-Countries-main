bb45bb4567490e32d28fccee0c970b1df056180b
lucascolm <lucaspaganelli20@gmail.com>
2022-08-29 13:28:13 -0300

    update landing page

--- client/src/Components/Landing/Landing.jsx
+++ client/src/Components/Landing/Landing.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import{Link} from 'react-router-dom'
-
+import s from "./Landing.module.css"
 export default function Landing() {
   return (
-    <div>Landing
-    <Link to="/home">
-    <button>Home</button>
+    <div className={s.landingPage}>
+    <h1 className={s.titulo}>Comienza tu viaje con un solo click</h1>
+    <Link className={s.linkLanding} to="/home">
+    <button className={s.landingbtn}>Comenzar</button>
     </Link>
     </div>
   )
