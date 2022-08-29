18dbed5215c6c61f098888c932d44aff4b8ae479
lucascolm <lucaspaganelli20@gmail.com>
2022-08-29 13:25:00 -0300

    creo funcion para el paginado

---
+++ client/src/Components/Home/funciones.js
@@ -0,0 +1,16 @@
+
+const paginado =  (paises)=>{
+  const arr=[]
+  const paisestwo=paises.slice(9,paises.length)
+  const maxpage= 1+ Math.ceil((paises.length-9)/10);
+  for(let i=0;i<maxpage;i++){
+    if(i===0){
+      arr.push({pageinfo:{currentpage:1,totalPages:maxpage,nextpage:maxpage>1?true:false},paises:paises.slice(0,9)})
+
+    }else{
+      arr.push({pageinfo:{currentpage:i+1,totalPages:maxpage,nextpage:maxpage>i+1?true:false},paises:paisestwo.slice((i-1)*10,i*10)})
+    }
+  } 
+  return arr;
+}
+export default paginado
