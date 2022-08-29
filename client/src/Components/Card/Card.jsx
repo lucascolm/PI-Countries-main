f9241b1c3207bba07c17640621668e5a476bac86
lucascolm <lucaspaganelli20@gmail.com>
2022-08-29 13:25:56 -0300

    update en componente card

--- client/src/Components/Card/Card.jsx
+++ client/src/Components/Card/Card.jsx
@@ -1,20 +1,20 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import s from "./Card.module.css"
 
-
-
- const Card = ({id,name,flag,continent}) => {
+const Card = ({ id, name, flag, continent }) => {
   return (
-    <Link to={`/home/countries/${id}`}className="linkToDetail">
-      <div key={id}>
-        <img src={flag} alt={'country-img'} className="flagImg" />
-        <div className="infoCard">
-        <h1 className="title">{name}</h1>
-        <p className=" continet">{continent}</p>
+    <Link to={`/home/countries/${id}`} className="linkToDetail">
+      <div className={s.card}>
+        <div className={s.cardContent} key={id}>
+          <img src={flag} alt={"country-img"} className={s.flagImg} />
+          <div className={s.infoCard}>
+            <h1  className={s.title}>{name}</h1>
+            <p className={s.continent}>Continente:{continent}</p>
+          </div>
         </div>
       </div>
     </Link>
-
-  )
-}
-export default Card
+  );
+};
+export default Card;
