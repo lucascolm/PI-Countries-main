60c1dd812e2f4a136d06eb985503f8ccc2278ed1
lucascolm <lucaspaganelli20@gmail.com>
2022-09-01 01:51:13 -0300

    despacho setpage

--- client/src/Components/SearchBar/SerchBar.jsx
+++ client/src/Components/SearchBar/SerchBar.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import { useState,useEffect } from 'react';
 import { useDispatch} from 'react-redux';
-import { Link } from 'react-router-dom';
-import {getCountryByName,removeSearch,cleanState} from '../../redux/actions'
+// import { Link } from 'react-router-dom';
+import {getCountryByName,removeSearch,changePage} from '../../redux/actions'
  const SerchBar = () => {
-
+  
   const [name,setName]=useState("")
   const dispatch=useDispatch();
 
   useEffect(()=>{
     if(name!==""){
+      dispatch(changePage(1))
     dispatch(getCountryByName(name))}
     else{
       dispatch(removeSearch())
