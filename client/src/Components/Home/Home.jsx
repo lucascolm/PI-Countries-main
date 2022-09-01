67b8c847e96550864c842c3a9cab1e86e37e0fd6
lucascolm <lucaspaganelli20@gmail.com>
2022-09-01 01:49:40 -0300

    agrego estado global para el paginado

--- client/src/Components/Home/Home.jsx
+++ client/src/Components/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getAllCountries, removeSearch } from "../../redux/actions";
+import { getAllCountries, removeSearch,getAllActivities, changePage } from "../../redux/actions";
 import Card from "../Card/Card";
 import paginado from "./funciones";
 import s from "./Home.module.css";
@@ -8,11 +8,14 @@ import s from "./Home.module.css";
 //import NavBar from '../NavBar/NavBar'
 
 const Home = () => {
+  let maxpage;
+  
+  const countriesFilter=useSelector((state) => state.countriesFilter)
   const auxCountries = useSelector((state) => state.allCountries);
   const countriesN = useSelector((state) => state.contriesByName);
+  const page=useSelector((state) =>state.actualPage);
   const dispatch = useDispatch();
-
-  const [page, setPage] = useState(1);
+  // const [page, setPage] = useState(1);
   //Paginado
 
   //console.log(firstOnPage,lastOnPage)
@@ -23,8 +26,29 @@ const Home = () => {
   useEffect(() => {
     dispatch(removeSearch());
     dispatch(getAllCountries());
+    dispatch(getAllActivities())
   }, [dispatch]);
 
+ function nextpage(e){
+
+  window.scrollTo(0,0)
+  if(page<maxpage){
+    dispatch(changePage(page+1))
+
+  }else{
+   alert("estas en la ultima pagina")
+  }
+ }
+
+
+ function prevpage(e){
+  if(page==1){
+    alert("estas en la primera pagina")
+  }else{
+    window.scrollTo(0,0)
+    dispatch(changePage(page-1))
+  }
+ }
   return (
     <div className={s.home}>
       {/* {!auxCountries?null:<NavBar/>} */}
@@ -47,7 +71,16 @@ const Home = () => {
               );
             })
           )
-        ) : auxCountries.length > 0 ? (
+        ):countriesFilter.length>0?paginado(countriesFilter)[page-1].paises.map((c) => (
+          <Card
+            key={c.id}
+            id={c.id}
+            name={c.name}
+            flag={c.flag}
+            continent={c.continent}
+          />
+        )) : auxCountries.length > 0 ? (
+
           paginado(auxCountries)[page - 1].paises.map((c) => (
             <Card
               key={c.id}
@@ -62,32 +95,53 @@ const Home = () => {
         )}
       </div>
       <div className={s.paginado}>
+      <button onClick={prevpage}>anterior</button>
         {countriesN.length > 0 && typeof countriesN !== "string"
           ? paginado(countriesN).map((e, index) => {
+            maxpage=e.pageinfo.totalPages
               return (
                 <button
                   onClick={() => {
-                    setPage(index + 1);
+                    window.scrollTo(0,0)
+                    dispatch(changePage(index+1))
                   }}
                 >
                   {index + 1}
                 </button>
               );
             })
-          : auxCountries.length > 0 &&
-            paginado(auxCountries).map((e, index) => {
-              return (
-                <button
-                  onClick={() => {
-                    setPage(index + 1);
-                  }}
-                >
+          :countriesFilter.length>0? paginado(countriesFilter).map((e, index) => {
+           maxpage=e.pageinfo.totalPages
+           
+            return (
+              <button
+                onClick={() => {
+                  window.scrollTo(0,0)
+                  dispatch(changePage(index+1))
+                }}
+              >
+                {index + 1}
+              </button>
+            );
+          }):auxCountries.length > 0 &&
+          paginado(auxCountries).map((e, index) => {
+            console.log(maxpage)
+            maxpage=e.pageinfo.totalPages
+            return (
+              <button
+              onClick={() => {
+                window.scrollTo(0,0)
+                dispatch(changePage(index+1))
+              }}
+              >
                   {index + 1}
                 </button>
               );
             })}
+            {/* console.log(e.pageinfo.totalPages) */}
+          <button onClick={nextpage}>siguiente</button>
       </div>
-    </div>
+    </div>  
   );
 };
 export default Home;
