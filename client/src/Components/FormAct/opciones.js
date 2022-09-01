954cb09aa3b4e472ee1505206f9cf65a80943cd1
lucascolm <lucaspaganelli20@gmail.com>
2022-09-01 01:44:37 -0300

    borro archivo

--- client/src/Components/FormAct/opciones.js
+++
@@ -1,7 +0,0 @@
-const opciones={
-  temporadas:[{ label: "Verano", value: "Summer" },
-  { label: "Otoño", value: "Autumn" },
-  { label:"Invierno", value: "Winter" },
-  { label: "Primavera", value: "Spring" },]
-}
-export default opciones;
