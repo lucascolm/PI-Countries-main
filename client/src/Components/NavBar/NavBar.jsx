cd4bcced3ae77a21e173021532018b349e2ed668
lucascolm <lucaspaganelli20@gmail.com>
2022-09-01 01:50:50 -0300

    agrego funcionalidades a mi filtro en el nav

--- client/src/Components/NavBar/NavBar.jsx
+++ client/src/Components/NavBar/NavBar.jsx
@@ -1,19 +1,73 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import { useDispatch,useSelector } from "react-redux";
 import { useState } from "react";
 import SerchBar from "../SearchBar/SerchBar";
 //import logo from "../../assets/logo.png";
 import { useHistory } from "react-router-dom";
 import s from "./NavBar.module.css";
-//import {cleanState}from "../../redux/actions/index"
+import { OrderByName,orderbypop,filterContinent,cleanState,filterByActivities,changePage } from "../../redux/actions";
+
 const NavBar = () => {
+  // const countries =useSelector((state)=>state.countriesFilter)
+  const allActivities=useSelector((state) => state.activities)
+
   const dispatch = useDispatch();
   const history =useHistory();
-  // const clear=()=>{
-  //   dispatch(cleanState())
-  
-  //  }
+  const [filter,setfilter]=useState({
+    order:"",
+    poblacion:"",
+    continente:"",
+    actividad:""
+  })
+  function clean(){
+    return dispatch(cleanState());
+  }
+  const handleChangeOrder=(e)=>{
+    console.log(e.target.value)
+   if (e.target.value==="ASC"||e.target.value==="DESC"){
+    dispatch(changePage(1))
+    dispatch(OrderByName(e.target.value))
+    setfilter({
+      ...filter,
+      order:e.target.value
+    })
+   }else if(e.target.value==="PASC"||e.target.value==="PDESC"){
+    dispatch(changePage(1))
+    dispatch(orderbypop(e.target.value))
+    setfilter({
+      ...filter,
+      poblacion:e.target.value
+    })
+  }else{
+    dispatch(changePage(1))
+    dispatch(filterContinent(e.target.value))
+    setfilter({
+      ...filter,
+      continente:e.target.value
+    })
+  }
+  }
+  const handleFilter=(e)=>{
+    dispatch(changePage(1))
+    dispatch(filterByActivities(e.target.value))
+    setfilter({
+      ...filter,
+      actividad:e.target.value
+    })
+  }
+
+
+  const clear=()=>{
+    dispatch(changePage(1))
+    dispatch(cleanState())
+    setfilter({
+      order:"",
+      poblacion:"",
+      continente:"",
+      actividad:""
+    })
+   }
   return (
     <div className={s.NavBar}>
       <div className={s.navUp}>
@@ -30,37 +84,48 @@ const NavBar = () => {
         </div>
       </div>
       <div className={s.SerchBar}>
-        <SerchBar />
+        <SerchBar className={s.search}/>
       </div>
       </div>
      <div className={s.selects}>
+      <div>
+        <select value={filter.actividad} onChange={handleFilter}>
+          <option hidden>Por actividad:</option>
+          {allActivities.length>0 &&
+           allActivities.map((activity)=>{
+            return <option key={activity.id} value={activity.name}>{activity.name}</option>
+            })}
+        </select>
+      </div>
         <div>
-          <select>
-            <option hidden>Por continente</option>
+          <select value={filter.continente} onChange={handleChangeOrder}>
+            <option hidden>Por continente: </option>
             <option value="Africa">Africa</option>
-            <option value="Antarctica">Antarctica</option>
+            <option value="Antarctic">Antartida</option>
             <option value="Asia">Asia</option>
-            <option value="Europe">Europe</option>
-            <option value="North America">North America</option>
+            <option value="Europe">Europa</option>
+            <option value="North America">America del norte</option>
             <option value="Oceania">Oceania</option>
-            <option value="Americas">Americas</option>
+            <option value="Americas">America del sur</option>
           </select>
         </div>
         <div>
-          <select>
-            <option hidden>Por orden</option>
+          <select value={filter.order} onChange={handleChangeOrder}>
+            <option hidden>Por orden: </option>
             <option value="ASC">A-Z</option>
             <option value="DESC">Z-A</option>
           </select>
         </div>
         <div>
-          <select>
-            <option hidden>Por poblacion</option>
+          <select value={filter.poblacion} onChange={handleChangeOrder}>
+            <option hidden>Por poblacion </option>
             <option value="PASC">Mayor poblacion</option>
             <option value="PDESC"> Menor poblacion</option>
           </select>
+       
         </div>
-      </div> 
+      <button className={s.Link} type="submit"  onClick={clear}>Todos los paises</button>
+      </div>  
     </div>
   );
 };
